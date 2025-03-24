265b22691fa13c3c7d3b7b3239beb6575f7ee64b
bkiritom8 <bhargavsp01@gmail.com>
2025-03-24 19:17:10 -0400

    added GameEngineApp

--- src/enginedriver/GameEngineApp.java
+++ src/enginedriver/GameEngineApp.java
@@ -1,4 +1,45 @@
 package enginedriver;
 
+import java.io.IOException;
+import model.GameWorld;
+import controller.GameController;
+
+/**
+ *
+ */
 public class GameEngineApp {
+  private final String gameFileName;
+  private final Readable input;
+  private final  Appendable output;
+
+  /**
+   *
+   * @param gameFileName
+   * @param input
+   * @param output
+   */
+  public GameEngineApp(String gameFileName, Readable input, Appendable output) {
+    this.gameFileName = gameFileName;
+    this.input = input;
+    this.output = output;
+  }
+
+  /**
+   *
+   * @throws IOException
+   */
+  public void  start() throws IOException {
+    try {
+      // Create the game model by loading the specified JSON
+      GameWorld gameWorld = new GameWorld(gameFileName);
+
+      // Create the controller, linking it to the model and I/O
+      GameController controller = new GameController(gameWorld, input, output);
+
+      // Start game loop
+      controller.play();
+    } catch (IOException e) {
+      throw new IOException("Error starting game", e.getMessage(), e);
+    }
+  }
 }
