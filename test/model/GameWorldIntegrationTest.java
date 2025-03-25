bb9c73b952cfdc47a57412a3e7bd31ee1f67860e
bkiritom8 <bhargavsp01@gmail.com>
2025-03-25 19:04:42 -0400

    added testPlayerNavigationScenario

--- test/model/GameWorldIntegrationTest.java
+++ test/model/GameWorldIntegrationTest.java
@@ -20,4 +20,31 @@ public class GameWorldIntegrationTest {
     assertEquals("Hallway 1", startRoom.getName());
   }
 
+  @Test
+  void testPlayerNavigationScenario() throws IOException {
+    GameWorld gameWorld = new GameWorld("./resources/empty_rooms.json");
+    Player player = gameWorld.getPlayer();
+
+    // Start in room 1
+    assertEquals("1", player.getCurrentRoom().getRoomNumber());
+
+    // Move north to room 2
+    Room currentRoom = player.getCurrentRoom();
+    Room northRoom = currentRoom.getExit(Direction.NORTH);
+    player.setCurrentRoom(northRoom);
+    assertEquals("2", player.getCurrentRoom().getRoomNumber());
+
+    // Move north to room 3
+    currentRoom = player.getCurrentRoom();
+    northRoom = currentRoom.getExit(Direction.NORTH);
+    player.setCurrentRoom(northRoom);
+    assertEquals("3", player.getCurrentRoom().getRoomNumber());
+
+    // Move south back to room 2
+    currentRoom = player.getCurrentRoom();
+    Room southRoom = currentRoom.getExit(Direction.SOUTH);
+    player.setCurrentRoom(southRoom);
+    assertEquals("2", player.getCurrentRoom().getRoomNumber());
+  }
+
 }
