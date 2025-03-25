75ce6adccc2afdf0b794b7870bc1256c27d2a713
bkiritom8 <bhargavsp01@gmail.com>
2025-03-25 19:03:01 -0400

    added testLoadEmptyRoomsGame

--- test/model/GameWorldIntegrationTest.java
+++ test/model/GameWorldIntegrationTest.java
@@ -1,4 +1,23 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *  Integration test for the game world.
+ */
 public class GameWorldIntegrationTest {
+
+  @Test
+  void testLoadEmptyRoomsGame() throws IOException {
+    GameWorld gameWorld = new GameWorld("./resources/empty_rooms.json");
+    assertEquals("Empty Rooms", gameWorld.getGameName());
+
+    Room startRoom = gameWorld.getPlayer().getCurrentRoom();
+    assertEquals("1", startRoom.getRoomNumber());
+    assertEquals("Hallway 1", startRoom.getName());
+  }
+
 }
